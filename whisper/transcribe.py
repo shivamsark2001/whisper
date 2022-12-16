777efa90159a7c5ff9c737d00a8773dcd37267f7
shivamsark2001 <87816749+shivamsark2001@users.noreply.github.com>
2022-12-16 17:29:13 +0530

    hindi default

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -87,13 +87,14 @@ def transcribe(
         if not model.is_multilingual:
             decode_options["language"] = "en"
         else:
-            if verbose:
-                print("Detecting language using up to the first 30 seconds. Use `--language` to specify the language")
-            segment = pad_or_trim(mel, N_FRAMES).to(model.device).to(dtype)
-            _, probs = model.detect_language(segment)
-            decode_options["language"] = max(probs, key=probs.get)
-            if verbose is not None:
-                print(f"Detected language: {LANGUAGES[decode_options['language']].title()}")
+            # if verbose:
+            #     print("Detecting language using up to the first 30 seconds. Use `--language` to specify the language")
+            # segment = pad_or_trim(mel, N_FRAMES).to(model.device).to(dtype)
+            # _, probs = model.detect_language(segment)
+            # decode_options["language"] = max(probs, key=probs.get)
+            # if verbose is not None:
+            #     print(f"Detected language: {LANGUAGES[decode_options['language']].title()}")
+            decode_options["language"] = "hi"
 
     language = decode_options["language"]
     task = decode_options.get("task", "transcribe")
